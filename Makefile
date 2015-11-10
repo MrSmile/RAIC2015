ad22f8197a27c1491de62b4e77d09e6910f73d80
Dr.Smile <vabnick@gmail.com>
2015-11-10 10:21:16 +0300

    Add basic infrastructure

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-CPPFLAGS:= -static -fno-optimize-sibling-calls -fno-strict-aliasing -DONLINE_JUDGE -D_LINUX -lm -s -x c++ -O2 -Wall -Wno-unknown-pragmas
+CPPFLAGS:= -std=c++11 -static -fno-optimize-sibling-calls -fno-strict-aliasing -DONLINE_JUDGE -D_LINUX -lm -s -x c++ -O2 -Wall -Wno-unknown-pragmas
 cpps:=$(shell find -name '*.cpp')
 objs:=$(patsubst %.cpp, %.o, $(cpps))
 progname:=MyStrategy
 
 .PHONY: all clean
 all: $(progname)
+
+clean:
+	rm -f $(objs)
 
 $(progname): $(objs)
 	g++ $^ -o $@
--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -1,22 +1,177 @@
 #include "MyStrategy.h"
 
-#define PI 3.14159265358979323846
-#define _USE_MATH_DEFINES
-
 #include <cmath>
 #include <cstdlib>
+#include <cassert>
+#include <algorithm>
+#include <limits>
+#include <map>
 
-using namespace model;
+#include <iostream>  // DEBUG
+#include <iomanip>  // DEBUG
+
 using namespace std;
 
-void MyStrategy::move(const Car& self, const World& world, const Game& game, Move& move) {
+
+constexpr double pi = 3.14159265358979323846264338327950288;
+
+
+inline double frand()
+{
+    return rand() * (2.0 / RAND_MAX) - 1;
+}
+
+
+inline constexpr double sqr(double x)
+{
+    return x * x;
+}
+
+inline constexpr double rem(double x, double y)
+{
+    return y * (x / y - floor(x / y));
+}
+
+inline constexpr double relAngle(double angle)
+{
+    return rem(angle + pi, 2 * pi) - pi;
+}
+
+
+struct Vec2D
+{
+    double x, y;
+
+    Vec2D()
+    {
+    }
+
+    constexpr Vec2D(const Vec2D &v) : x(v.x), y(v.y)
+    {
+    }
+
+    constexpr Vec2D(double x_, double y_) : x(x_), y(y_)
+    {
+    }
+
+    Vec2D &operator = (const Vec2D &v)
+    {
+        x = v.x;  y = v.y;  return *this;
+    }
+
+    constexpr Vec2D operator + (const Vec2D &v) const
+    {
+        return Vec2D(x + v.x, y + v.y);
+    }
+
+    Vec2D &operator += (const Vec2D &v)
+    {
+        x += v.x;  y += v.y;  return *this;
+    }
+
+    constexpr Vec2D operator - (const Vec2D &v) const
+    {
+        return Vec2D(x - v.x, y - v.y);
+    }
+
+    Vec2D &operator -= (const Vec2D &v)
+    {
+        x -= v.x;  y -= v.y;  return *this;
+    }
+
+    constexpr Vec2D operator - () const
+    {
+        return Vec2D(-x, -y);
+    }
+
+    constexpr Vec2D operator * (double a) const
+    {
+        return Vec2D(a * x, a * y);
+    }
+
+    Vec2D &operator *= (double a)
+    {
+        x *= a;  y *= a;  return *this;
+    }
+
+    constexpr double operator * (const Vec2D &v) const
+    {
+        return x * v.x + y * v.y;
+    }
+
+    constexpr Vec2D operator / (double a) const
+    {
+        return (*this) * (1 / a);
+    }
+
+    Vec2D operator /= (double a)
+    {
+        return (*this) *= (1 / a);
+    }
+
+    constexpr Vec2D operator ~ () const
+    {
+        return Vec2D(y, -x);
+    }
+
+    constexpr double operator % (const Vec2D &v) const
+    {
+        return *this * ~v;
+    }
+
+    constexpr double sqr() const
+    {
+        return x * x + y * y;
+    }
+
+    constexpr double len() const
+    {
+        return std::sqrt(x * x + y * y);
+    }
+};
+
+inline constexpr Vec2D operator * (double a, const Vec2D &v)
+{
+    return v * a;
+}
+
+inline constexpr Vec2D normalize(const Vec2D &v)
+{
+    return v / v.len();
+}
+
+inline constexpr Vec2D sincos(double angle)
+{
+    return Vec2D(cos(angle), sin(angle));
+}
+
+inline constexpr Vec2D sincos_fast(float angle)
+{
+    return Vec2D(cos(angle), sin(angle));
+}
+
+inline constexpr Vec2D rotate(const Vec2D &v1, const Vec2D &v2)
+{
+    return Vec2D(v1.x * v2.x - v1.y * v2.y, v1.x * v2.y + v1.y * v2.x);
+}
+
+inline constexpr Vec2D conj(const Vec2D &v)
+{
+    return Vec2D(v.x, -v.y);
+}
+
+
+
+void MyStrategy::move(const model::Car& self, const model::World& world, const model::Game& game, model::Move& move)
+{
     move.setEnginePower(1.0);
     move.setThrowProjectile(true);
     move.setSpillOil(true);
 
-    if (world.getTick() > game.getInitialFreezeDurationTicks()) {
+    if (world.getTick() > game.getInitialFreezeDurationTicks())
         move.setUseNitro(true);
-    }
 }
 
-MyStrategy::MyStrategy() { }
+MyStrategy::MyStrategy()
+{
+}
